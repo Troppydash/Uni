21adabe889689edd15c038e7e19d97750627efaa
troppydash <troppydash.developer@gmail.com>
2024-03-10 23:50:10 +1100

    OOP Workshop Week 3

--- OOP/workshops/HelloWorld/src/Program.java
+++ OOP/workshops/HelloWorld/src/Program.java
@@ -2,5 +2,12 @@ public class Program {
     public static void main(String[] args) {
         Problems.problem_1_1(5);
         Problems.problem_1_2(7);
+
+
+        Test test = new Test();
     }
+}
+
+class Test {
+    protected int what;
 }
