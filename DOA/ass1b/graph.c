6a274457d326760a8607110e8ec3fc4a6fca5b68
troppydash <troppydash.developer@gmail.com>
2024-03-19 23:02:35 +1100

    Finished part a

---
+++ DOA/ass1b/graph.c
@@ -0,0 +1,240 @@
+/*
+graph.c
+
+Set of vertices and edges implementation.
+
+Implementations for helper functions for graph construction and manipulation.
+
+Skeleton written by Grady Fitzpatrick for COMP20007 Assignment 1 2024
+*/
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <float.h>
+#include <math.h>
+#include <stdbool.h>
+#include "graph.h"
+#include "utils.h"
+#include "pq.h"
+#include "list.h"
+
+#define INITIALEDGES 32
+
+struct edge;
+
+/* Definition of a graph. */
+struct graph {
+    int numVertices;
+    int numEdges;
+    int allocedEdges;
+    struct edge **edgeList;
+};
+
+/* Definition of an edge. */
+struct edge {
+    int start;
+    int end;
+    int cost;
+};
+
+struct graph *newGraph(int numVertices) {
+    struct graph *g = (struct graph *) malloc(sizeof(struct graph));
+    assert(g);
+    /* Initialise edges. */
+    g->numVertices = numVertices;
+    g->numEdges = 0;
+    g->allocedEdges = 0;
+    g->edgeList = NULL;
+    return g;
+}
+
+/* Adds an edge to the given graph. */
+void addEdge(struct graph *g, int start, int end, int cost) {
+    assert(g);
+    struct edge *newEdge = NULL;
+    /* Check we have enough space for the new edge. */
+    if ((g->numEdges + 1) > g->allocedEdges) {
+        if (g->allocedEdges == 0) {
+            g->allocedEdges = INITIALEDGES;
+        } else {
+            (g->allocedEdges) *= 2;
+        }
+        g->edgeList = (struct edge **) realloc(g->edgeList,
+                                               sizeof(struct edge *) * g->allocedEdges);
+        assert(g->edgeList);
+    }
+
+    /* Create the edge */
+    newEdge = (struct edge *) malloc(sizeof(struct edge));
+    assert(newEdge);
+    newEdge->start = start;
+    newEdge->end = end;
+    newEdge->cost = cost;
+
+    /* Add the edge to the list of edges. */
+    g->edgeList[g->numEdges] = newEdge;
+    (g->numEdges)++;
+}
+
+/* Returns a new graph which is a deep copy of the given graph (which must be
+  freed with freeGraph when no longer used). */
+struct graph *duplicateGraph(struct graph *g) {
+    struct graph *copyGraph = (struct graph *) malloc(sizeof(struct graph));
+    assert(copyGraph);
+    copyGraph->numVertices = g->numVertices;
+    copyGraph->numEdges = g->numEdges;
+    copyGraph->allocedEdges = g->allocedEdges;
+    copyGraph->edgeList = (struct edge **) malloc(sizeof(struct edge *) * g->allocedEdges);
+    assert(copyGraph->edgeList || copyGraph->numEdges == 0);
+    int i;
+    /* Copy edge list. */
+    for (i = 0; i < g->numEdges; i++) {
+        struct edge *newEdge = (struct edge *) malloc(sizeof(struct edge));
+        assert(newEdge);
+        newEdge->start = (g->edgeList)[i]->start;
+        newEdge->end = (g->edgeList)[i]->end;
+        newEdge->cost = (g->edgeList)[i]->cost;
+        (copyGraph->edgeList)[i] = newEdge;
+    }
+    return copyGraph;
+}
+
+/* Frees all memory used by graph. */
+void freeGraph(struct graph *g) {
+    int i;
+    for (i = 0; i < g->numEdges; i++) {
+        free((g->edgeList)[i]);
+    }
+    if (g->edgeList) {
+        free(g->edgeList);
+    }
+    free(g);
+}
+
+struct pair {
+    int first;
+    int second;
+};
+
+// compute the shortest path in a graph from two points, returns the minimum distance
+static int shortestPath(struct list **adjList, int vertices, int startingLocation, int finalLocation) {
+    // allocate visited array
+    bool *visited = malloc(sizeof(bool) * vertices);
+    assert(visited);
+    for (int i = 0; i < vertices; ++i) {
+        visited[i] = false;
+    }
+
+    // a queue, maximize size is vertices for each vertex can be added to the queue exactly once
+    // queue elements are of the form (vertex, depth)
+    struct pair *queue = malloc(sizeof(struct pair) * vertices);
+    assert(queue);
+    int start = 0;
+    int next = 0;
+
+    // add starting vertex
+    struct pair init = {startingLocation, 0};
+    visited[next] = true;
+    queue[next++] = init;
+
+    int result = -1;
+    // while the queue is not empty
+    while (start < next) {
+        // get front with sanity check
+        assert(start < vertices);
+        struct pair front = queue[start++];
+        int node = front.first;
+        int depth = front.second;
+
+        // check if reached destination
+        if (node == finalLocation) {
+            result = depth;
+            break;
+        }
+
+        // iterate through its neighbours
+        struct list *head = adjList[node];
+        while (head != NULL) {
+            struct pair *neighbour = (struct pair *) peekHead(head);
+            int other = neighbour->first;
+            head = nextHead(head);
+
+            // ignore if visited
+            if (visited[other]) {
+                continue;
+            }
+            // pre-visit the node and append to queue
+            visited[other] = true;
+
+            // sanity check to make sure no buffer overflow
+            assert(next < vertices);
+            struct pair data = {other, depth + 1};
+            queue[next++] = data;
+
+        }
+    }
+
+    free(queue);
+    free(visited);
+
+    // returns -1 if not found
+    return result;
+}
+
+
+struct solution *graphSolve(struct graph *g, enum problemPart part,
+                            int numLocations, int startingLocation, int finalLocation) {
+    struct solution *solution = (struct solution *)
+            malloc(sizeof(struct solution));
+    assert(solution);
+
+    int vertices = g->numVertices;
+    int edges = g->numEdges;
+
+    // create adjacency list
+    struct list **adjList = malloc(vertices * sizeof(struct list *));
+    assert(adjList);
+    for (int i = 0; i < vertices; ++i) {
+        adjList[i] = NULL;
+    }
+
+    // add all edges into the list, undirected
+    for (int i = 0; i < edges; ++i) {
+        struct edge *e = g->edgeList[i];
+
+        struct pair *forward = malloc(sizeof(struct pair));
+        assert(forward);
+        forward->first = e->end;
+        forward->second = e->cost;
+
+        struct pair *reverse = malloc(sizeof(struct pair));
+        assert(reverse);
+        reverse->first = e->start;
+        reverse->second = e->cost;
+
+        adjList[e->start] = prependList(adjList[e->start], forward);
+        adjList[e->end] = prependList(adjList[e->end], reverse);
+    }
+
+    if (part == PART_A) {
+        solution->damageTaken = shortestPath(adjList, vertices, startingLocation, finalLocation);
+    } else if (part == PART_B) {
+        /* IMPLEMENT 2B SOLUTION HERE */
+        solution->totalCost = -1;
+    } else if (part == PART_C) {
+        /* IMPLEMENT 2C SOLUTION HERE */
+        solution->artisanCost = -1;
+    } else {
+        /* IMPLEMENT 2D SOLUTION HERE */
+        solution->totalPercentage = -1;
+    }
+
+
+    // free adjacency list
+    for (int i = 0; i < vertices; ++i) {
+        freeList(adjList[i]);
+    }
+    free(adjList);
+
+    return solution;
+}
---
+++ DOA/ass1b/readme.md
@@ -0,0 +1,1 @@
+just graph.c
