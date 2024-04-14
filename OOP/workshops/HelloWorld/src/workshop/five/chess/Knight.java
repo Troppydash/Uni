8982ac9f121e1ae15c6ebc63d03ccdb57f6c7448
troppydash <troppydash.developer@gmail.com>
2024-04-14 18:17:55 +1000

    Changed OOP to submodules

--- OOP/workshops/HelloWorld/src/workshop/five/chess/Knight.java
+++
@@ -1,9 +0,0 @@
-package workshop.five.chess;
-
-public class Knight {
-    protected boolean isValidMove(int toRow, int toColumn) {
-        boolean isValid = false;
-        // Code to check if the move is valid and set isValid
-        return isValid;
-    }
-}
